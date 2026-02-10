0976a0293b22c65c59bfcddc7b462b5db593e95d
Dapaa1105 <dafanr2307@gmail.com>
2026-02-10 10:27:10 +0700

    Modifikasi Praktikum Array 1

--- Jobsheet1/praktikumArray05.java
+++ Jobsheet1/praktikumArray05.java
@@ -4,34 +4,35 @@ import java.util.Scanner;
 
 public class praktikumArray05 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        String[] mk = {
-                "Pancasila",
-                "Konsep Teknologi Informasi",
-                "Critical Thinking dan Problem Solving",
-                "Matematika Dasar",
-                "Bahasa Inggris",
-                "Dasar Pemrograman",
-                "Praktikum Dasar Pemrograman",
-                "Keselamatan dan Kesehatan Kerja"
-        };
+        System.out.print("Masukkan Jumlah Mata Kuliah: ");
+        int jumlahMK = input.nextInt();
+        input.nextLine();
 
-        int[] sks = { 2, 2, 2, 3, 2, 3, 1, 2 };
-
-        double[] nilaiAngka = new double[mk.length];
-        String[] nilaiHuruf = new String[mk.length];
-        double[] bobot = new double[mk.length];
+        String[] mk = new String[jumlahMK];
+        int[] sks = new int[jumlahMK];
+        double[] nilaiAngka = new double[jumlahMK];
+        String[] nilaiHuruf = new String[jumlahMK];
+        double[] bobot = new double[jumlahMK];
 
         double totalNilai = 0;
         int totalSKS = 0;
 
+        System.out.println();
         System.out.println("Program Menghitung IP Semester");
         System.out.println("==============================");
 
-        for (int i = 0; i < mk.length; i++) {
-            System.out.print("masukkan nilai Angka untuk MK " + mk[i] + ": ");
-            nilaiAngka[i] = sc.nextDouble();
+        for (int i = 0; i < jumlahMK; i++) {
+            System.out.print("Masukkan Nama Mata Kuliah: ");
+            mk[i] = input.nextLine();
+
+            System.out.print("Masukkan SKS Mata Kuliah: ");
+            sks[i] = input.nextInt();
+
+            System.out.print("Masukkan Nilai Angka Untuk Mata Kuliah " + mk[i] + ": ");
+            nilaiAngka[i] = input.nextDouble();
+            input.nextLine();
 
             if (nilaiAngka[i] >= 80) {
                 nilaiHuruf[i] = "A";
@@ -55,15 +56,17 @@ public class praktikumArray05 {
 
             totalNilai = totalNilai + (bobot[i] * sks[i]);
             totalSKS = totalSKS + sks[i];
+            System.out.println();
         }
 
-        System.out.println();
+        System.out.println("==============================");
         System.out.println("Hasil Konversi Nilai");
         System.out.println("==============================");
+
         System.out.printf("%-40s %-12s %-12s %-12s\n",
                 "MK", "Nilai Angka", "Nilai Huruf", "Bobot Nilai");
 
-        for (int i = 0; i < mk.length; i++) {
+        for (int i = 0; i < jumlahMK; i++) {
             System.out.printf("%-40s %-12.2f %-12s %-12.2f\n",
                     mk[i], nilaiAngka[i], nilaiHuruf[i], bobot[i]);
         }
@@ -72,6 +75,5 @@ public class praktikumArray05 {
 
         System.out.println("==============================");
         System.out.printf("IP : %.2f\n", ip);
-        sc.close();
     }
 }
