ba1bc2ed113e138938aecc87b04bfee7b1fa44b0
Dapaa1105 <dafanr2307@gmail.com>
2026-02-24 08:53:45 +0700

    Praktikum 3

--- Jobsheet2/MahasiswaMain05.java
+++ Jobsheet2/MahasiswaMain05.java
@@ -2,7 +2,7 @@ package Jobsheet2;
 public class MahasiswaMain05 {
     public static void main(String[] args) {
 
-        Mahasiswa05 mhs1 = new Mahasiswa05();
+        Mahasiswa05 mhs1 = new Mahasiswa05("Muhammad Ali Farhan", "2241720171", 3.55, "SI 2J");
         mhs1.nim = "2241720171";
         mhs1.nama = "Muhammad Ali Farhan";
         mhs1.kelas = "SI 2J";
@@ -12,5 +12,11 @@ public class MahasiswaMain05 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa05 mhs2 = new Mahasiswa05("Annisa Nabila", "214720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
+
+
     }
 }
