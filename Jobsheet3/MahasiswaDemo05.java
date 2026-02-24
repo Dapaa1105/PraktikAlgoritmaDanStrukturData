29da943b7ab07b944406f554fe4b9f5bc27f8458
Dapaa1105 <dafanr2307@gmail.com>
2026-02-24 10:40:14 +0700

    Praktikum 1

--- Jobsheet3/MahasiswaDemo05.java
+++ Jobsheet3/MahasiswaDemo05.java
@@ -9,17 +9,17 @@ public class MahasiswaDemo05 {
         arrayofMahasiswa05[0].kelas = "SIB-1E";
         arrayofMahasiswa05[0].ipk = (float) 3.75;
 
-        arrayofMahasiswa05[0] = new Mahasiswa05();
-        arrayofMahasiswa05[0].nim = "2341720172";
-        arrayofMahasiswa05[0].nama = "ACHMAD MAULANA HAMZAH";
-        arrayofMahasiswa05[0].kelas = "TI-2A";
-        arrayofMahasiswa05[0].ipk = (float) 3.36;
+        arrayofMahasiswa05[1] = new Mahasiswa05();
+        arrayofMahasiswa05[1].nim = "2341720172";
+        arrayofMahasiswa05[1].nama = "ACHMAD MAULANA HAMZAH";
+        arrayofMahasiswa05[1].kelas = "TI-2A";
+        arrayofMahasiswa05[1].ipk = (float) 3.36;
 
-        arrayofMahasiswa05[0] = new Mahasiswa05();
-        arrayofMahasiswa05[0].nim = "244107023306";
-        arrayofMahasiswa05[0].nama = "DIRHAMAWAN PUTRANTO";
-        arrayofMahasiswa05[0].kelas = "TI-2E";
-        arrayofMahasiswa05[0].ipk = (float) 3.80;
+        arrayofMahasiswa05[2] = new Mahasiswa05();
+        arrayofMahasiswa05[2].nim = "244107023306";
+        arrayofMahasiswa05[2].nama = "DIRHAMAWAN PUTRANTO";
+        arrayofMahasiswa05[2].kelas = "TI-2E";
+        arrayofMahasiswa05[2].ipk = (float) 3.80;
 
         System.out.println("NIM    : " + arrayofMahasiswa05[0].nim);
         System.out.println("Nama   : " + arrayofMahasiswa05[0].nama);
