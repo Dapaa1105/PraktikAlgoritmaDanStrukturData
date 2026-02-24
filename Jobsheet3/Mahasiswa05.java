983ce1f472f0392c1d68e0cc6f85fe3ab84584a5
Dapaa1105 <dafanr2307@gmail.com>
2026-02-24 11:19:33 +0700

    Praktikum 3 Menjawab Pertanyaan

--- Jobsheet3/Mahasiswa05.java
+++ Jobsheet3/Mahasiswa05.java
@@ -1,9 +1,16 @@
 package Jobsheet3;
 
-public class Mahasiswa05{
+public class Mahasiswa05 {
     public String nim;
     public String nama;
     public String kelas;
     public float ipk;
-}
 
+    void cetakInfo() {
+        System.out.println("NIM  : " + nim);
+        System.out.println("Nama : " + nama);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK  : " + ipk);
+        System.out.println("-----------------------");
+    }
+}
