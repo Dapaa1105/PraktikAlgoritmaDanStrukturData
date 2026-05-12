7ddd9c5b741aa45a3df6ff650e7eacc7a169a2b7
Dapaa1105 <dafanr2307@gmail.com>
2026-05-12 11:42:50 +0700

    Progres CM 2

---
+++ CM2/Pembeli05.java
@@ -0,0 +1,13 @@
+package CM2;
+
+public class Pembeli05 {
+    String namaPembeli;
+    String noHp;
+    int noAntrean;
+
+    public Pembeli05(int noAntrean, String nama, String hp) {
+        this.noAntrean = noAntrean;
+        this.namaPembeli = nama;
+        this.noHp = hp;
+    }
+}
---
+++ CM2/Pesanan05.java
@@ -0,0 +1,13 @@
+package CM2;
+
+public class Pesanan05 {
+    int kodePesanan;
+    String namaPesanan;
+    int harga;
+
+    public Pesanan05(int kode, String nama, int harga) {
+        this.kodePesanan = kode;
+        this.namaPesanan = nama;
+        this.harga = harga;
+    }
+}
