fd7afd59ef64f3fd7f627903e92f792157f6bb0b
Dapaa1105 <dafanr2307@gmail.com>
2026-06-02 08:18:52 +0700

    Persiapan Jobsheet 14-2

---
+++ Praktikum14/Book05.java
@@ -0,0 +1,17 @@
+package Praktikum14;
+
+public class Book05 {
+    public String isbn;
+    public String title;
+    public Book05(){
+    }
+
+    public Book05(String isbn, String title) {
+        this.isbn = isbn;
+        this.title = title;
+    }
+    
+    public String toString(){
+        return "ISBN: " + this.isbn + " Title: " + this.title;
+    }
+}
